475ca68801ddce97fc5cdadebf96f9b3ebcad0ee
Chriscodinglife <chrisdotorellana@gmail.com>
2022-09-04 20:07:30 -0400

    Created a script to highlight steps of uploading app to ibm cloud

---
+++ how_to_push_app_to_ibm.sh
@@ -0,0 +1,16 @@
+# The following are basic instructions of pushing a local app up to IBM Cloud
+ibmcloud login -u <your email>
+# Set the region and the owner
+ibmcloud target --cf-api https://api.REGION.cf.cloud.ibm.com -r REGION -o OWNER
+# Create an account space called translator
+ibmcloud account space-create translator
+# Set the target to the account space
+ibmcloud target -s translator
+
+## Change the manifest file to a name for your app
+
+# Deploy your app
+ibmcloud app push
+
+# Check logs for your app
+ibmcloud cf logs <appname> --recent
